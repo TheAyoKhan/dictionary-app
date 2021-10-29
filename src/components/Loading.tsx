a39562b78ce05dd41f9202b508e9b8973d4eabd7
Ayaan Khan <theayokhan@gmail.com>
2021-10-28 20:47:59 -0400

    feat: added a loading animation (spinny thing) for before the page loads

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as LoadingIconSvg } from '../loading-icon.svg';
+
+const StyledLoading = styled.div`
+	.loading-icon {
+		fill: #222;
+		height: 100px;
+	}
+`;
 
 const Loading = (): JSX.Element => {
-	return <div className="loading">Loading...</div>;
+	return (
+		<StyledLoading className="loading">
+			<LoadingIconSvg className="loading-icon" />
+		</StyledLoading>
+	);
 };
 
 export default Loading;
