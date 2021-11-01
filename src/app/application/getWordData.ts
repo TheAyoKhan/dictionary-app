c8044e67769313562d895ff6cce5a9132ac4df74
Ayaan Khan <theayokhan@gmail.com>
2021-10-31 21:36:50 -0400

    feat/fix: Altered the src/app directory
    made the api endpoint (url) a parameter passed to the getWordData function

--- src/app/application/getWordData.ts
+++ src/app/application/getWordData.ts
@@ -1,9 +1,10 @@
 export const getWordData = async (
+	apiEndpoint: string,
 	word: string,
 	requester: (url: string) => Promise<Response>
 ): Promise<Response | null> => {
 	const response = await requester(
-		`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+		`${apiEndpoint}/${word}`
 	);
 	if (response) return response;
 	else return null;
--- src/app/interfaces/parseWordData.ts
+++ src/app/interfaces/parseWordData.ts
@@ -2,9 +2,9 @@ import { getWordData } from '../application/getWordData';
 import { WordData } from '../domain/wordData';
 
 export const parseWordData = async (
-	word: string
+	word: string,
 ): Promise<WordData[] | undefined> => {
-	const res = await getWordData(word, fetch);
+	const res = await getWordData('https://api.dictionaryapi.dev/api/v2/entries/en', word, fetch);
 	if (res?.ok) return res.json();
 	return;
 };
