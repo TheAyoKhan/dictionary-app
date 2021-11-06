8ee3ab96b743b14a273455428df38cf6386e7814
Ayaan Khan <theayokhan@gmail.com>
2021-11-05 21:30:00 -0400

    feat: Added a font
    Added a font, GlacialIndifference, with bold and italic alternate styles. This will replace a few fonts on the page eventually

--- src/fonts/GlobalFonts.tsx
+++ src/fonts/GlobalFonts.tsx
@@ -1,10 +1,30 @@
 import { createGlobalStyle } from "styled-components";
-import LeagueSpartan from '../fonts/LeagueSpartan-Bold.woff2';
+import LeagueSpartan from './LeagueSpartan-Bold.woff2';
+import GlacialIndifferenceRegular from './GlacialIndifference-Regular.woff2';
+import GlacialIndifferenceBold from './GlacialIndifference-Bold.woff2';
+import GlacialIndifferenceItalic from './GlacialIndifference-Italic.woff2';
 
 export default createGlobalStyle`
   @font-face {
     font-family: 'League Spartan';
     src: url(${LeagueSpartan}) format('woff2');
     font-weight: bold;
+  }
+
+  @font-face {
+    font-family: 'Glacial Indifference';
+    src: url(${GlacialIndifferenceRegular}) format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Glacial Indifference';
+    src: url(${GlacialIndifferenceBold}) format('woff2');
+    font-weight: bold;
+  }
+
+  @font-face {
+    font-family: 'Glacial Indifference';
+    src: url(${GlacialIndifferenceItalic}) format('woff2');
+    font-style: italic;
   }
 `;
