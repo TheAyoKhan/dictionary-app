10fa0e7c323cf2782a12ca457c28ef37dc627c95
Ayaan Khan <theayokhan@gmail.com>
2021-12-22 20:55:54 -0500

    fix(imports): Removed outdated imports for styled-components
    Removed outdated imports for styled-imports by removing GlobalFonts.tsx, and references to that GlobalFonts.tsx. The fonts were unneeded as Roboto will be used for most or all components.

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import CssBaseline from '@mui/material/CssBaseline';
 // Non-npm imports
 import App from './App';
-import GlobalFonts from '../fonts/GlobalFonts';
 import theme from '../theme';
 import { DarkModeContextProvider } from '../context/DarkModeContext';
 
 const AppWrapper = (): JSX.Element => {
 	return (
 		<ThemeProvider theme={theme}>
+			<CssBaseline />
 			<DarkModeContextProvider>
-				<GlobalFonts />
 				<App />
 			</DarkModeContextProvider>
 		</ThemeProvider>
